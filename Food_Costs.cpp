cbf1fb9fe61bbfc09931dd2235ff477a9d3ffcb2
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-26 20:39:22 +0600

    Difficulty-183 Problem-FOODCOST

---
+++ Food_Costs.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/FOODCOST
+int main(){
+    int a,b; cin>>a>>b;
+    int meal = a*6;
+    cout<<meal+b;  
+    return 0;
+}
