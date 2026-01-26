6c2d3cefda2afc138dc0fc285a63ebd38d370f2b
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-26 17:14:44 +0600

    AICOM

---
+++ AI_is_Coming.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/AICOM
+int main(){
+    int n ; cin>>n;
+    if(60>=n){
+        cout<<"YES";
+    }  
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
