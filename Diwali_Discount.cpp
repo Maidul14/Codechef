8c52c870bc2016ab94b294c9517b0cec94b79c71
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-26 20:28:44 +0600

    Difficulty-180 Problem-DIWALIDISC

---
+++ Diwali_Discount.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/DIWALIDISC
+int main(){
+      int A,B; cin>>A>>B;
+      if(A>B){
+        cout<<A-B;
+      }
+      else{
+        cout<<0;
+      }
+    return 0;
+}
