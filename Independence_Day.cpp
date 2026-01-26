1e46cf479222c7db599203cce6cfed84737344c2
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-26 19:59:49 +0600

    Difficulty-173 Problem-INDDAY

---
+++ Independence_Day.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/INDDAY
+int main() {
+	int day ; cin>>day;
+	if(15<day){
+	    cout<<-1;
+	}
+	else{
+	    cout<<15-day;
+	}
+
+}
