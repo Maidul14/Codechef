711906082a6a6e0bd4973454618a5f1cf0110220
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-26 19:51:58 +0600

    Difficulty-173 Problem: BAKECAKE1

---
+++ Chef_Bakes_Cake_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link: https://www.codechef.com/problems/BAKECAKE1
+int main(){
+    int n, s ; cin>>n>>s;
+    int pro = s*50;
+    int make = n*30;
+    int calculation = pro-make;
+    cout<<calculation;
+}
