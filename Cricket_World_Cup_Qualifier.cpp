d2550e65e9174719ba48e039a1e0242eeb5b5ce6
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 16:58:41 +0600

    Difficulty-203 Problem-CWC23QUALIF

---
+++ Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CWC23QUALIF
+int main(){
+    int n ; cin>>n;
+    if(n>=12){
+        cout<<"Yes";
+    }  
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
