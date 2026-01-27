ee05d879ef4d8ce8afcfa56bba3a269df5952c73
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 20:24:13 +0600

    Difficulty-242 Problem-MANGOLASSI

---
+++ Summer_Time.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/MANGOLASSI
+int main(){
+    int x; cin>>x;
+    if(x>35){
+        cout<<"YES";
+    } 
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
