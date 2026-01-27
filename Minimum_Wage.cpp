b973730ff0cfebf8cd28cd9ca645b19aff4ed2b0
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-28 02:40:55 +0600

    Difficulty-247 problem-MINWAGE

---
+++ Minimum_Wage.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/MINWAGE
+int main(){
+    int a ; cin>>a;
+    if(a>11){
+        cout<<"YES";
+    }
+    else cout<<"NO";
+    return 0;
+}
