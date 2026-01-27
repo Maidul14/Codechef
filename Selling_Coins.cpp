973f2cf25131793ec412f97fce3e66882c6fcffe
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 18:18:45 +0600

    Difficulty-218 Problem-SELLCOIN

---
+++ Selling_Coins.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/SELLCOIN
+int main(){
+    int a,b ; cin>>a>>b;
+    cout<<a+(b*2);
+    return 0;
+}
