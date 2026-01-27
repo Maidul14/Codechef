8a33a57d7be4b12245fb7f5298ada45d547919dd
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 18:51:37 +0600

    Difficulty-226 Problem-ICECONE

---
+++ Icecream_and_Cone.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/ICECONE
+int main(){
+    int x,y; cin>>x>>y;
+    int res = min(x,y);
+    cout<<res;
+    return 0;
+}
