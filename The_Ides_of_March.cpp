ed2b285f5141f2423db140b4346e6c83902ca979
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 19:22:20 +0600

    Difficulty-231 Problem-IDESM

---
+++ The_Ides_of_March.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/IDESM
+int main(){
+    int n; cin>>n;
+    if(n==15){
+        cout<<"Yes";
+    }  
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
