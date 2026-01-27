4f842e24cd568f0a16027a97988a016ed63f70da
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 17:30:20 +0600

    Difficulty-213 Problem-LUCKYSEVEN

---
+++ Lucky_Seven.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/LUCKYSEVEN
+int main(){
+    string s ; cin>>s;
+    cout<<s[6];  
+    return 0;
+}
