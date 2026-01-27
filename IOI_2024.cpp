18f8865fadb1f2fbd864e0f90c4b936661876c52
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 18:26:44 +0600

    Difficulty-219 Problem-IOI2024

---
+++ IOI_2024.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem libnk : https://www.codechef.com/problems/IOI2024
+int main(){
+      int x ; cin>>x;
+      if((1<=x)&& (8>=x)){
+        cout<<"YES";
+      }
+      else{
+        cout<<"NO";
+      }
+    return 0;
+}
