ac6e923cbdf0f3b012f942f5640b28cf1ccd1361
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 17:01:19 +0600

    Difficulty-205 Problem-CHRISTGREET

---
+++ Christmas_Greetings.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link: https://www.codechef.com/problems/CHRISTGREET
+int main(){
+    int x ; cin>>x;
+    if(x==25){
+        cout<<"CHRISTMAS";
+    }
+    else{
+        cout<<"ORDINARY";
+    }
+    return 0;
+}
