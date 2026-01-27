a3352e907fceb9f14fb60403048a202caacd8793
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 18:08:52 +0600

    Difficulty-217 Problem-INPL

---
+++ IPL.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link: https://www.codechef.com/problems/INPL
+int main(){
+    int x ; cin>>x;
+    if(x>=7){
+        cout<<"THALA";
+    }  
+    else{
+        cout<<"BOOM";
+    }
+    return 0;
+}
