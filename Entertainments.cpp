ef8183486fa101ebc76f1d97397bd81b330e78a1
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 16:55:03 +0600

    Difficulty-200 Problem:ENTERTAIN

---
+++ Entertainments.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/ENTERTAIN
+int main(){
+    int n ; cin>>n;
+    if(5>n){
+        cout<<n*200;
+    } 
+    else{
+        cout<<1000;
+    }
+    return 0;
+}
