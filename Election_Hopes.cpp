1b965a74d1676d8eff082e7c18dd6e802e9f599f
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-28 02:37:53 +0600

    Difficulty-245 Problem-ELHP

---
+++ Election_Hopes.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+//https://www.codechef.com/problems/ELHP
+int main(){
+      int x,y ; cin>>x>>y;
+    int chefu= y*2;
+    if(x>=chefu) cout<<"Yes";
+    else cout<<"No";
+    return 0;
+}
