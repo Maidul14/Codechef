0786b85805a380c6aeaab1a4026e6faa81809e2f
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-29 02:41:36 +0600

    Difficulty-252 Problem-TAXSAVING

---
+++ Saving_Taxes.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link: https://www.codechef.com/problems/TAXSAVING
+int main(){
+    int test ; cin>>test;
+    while (test--)
+    {
+        int x,y; cin>>x>>y;
+        cout<<x-y<<endl;
+    }  
+    return 0;
+}
