858329a75b0ff147387bca3228bfc6992d2992f8
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-29 18:20:23 +0600

    Difficulty-267 Problem-DETSCORE

---
+++ Determine_the_Score.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int test ; cin>>test;
+    while (test--)
+    {
+        int x,n ; cin>>x>>n;
+        int res  = x/10 ;
+        cout<<res*n<<endl;
+
+    }
+    
+    return 0;
+}
