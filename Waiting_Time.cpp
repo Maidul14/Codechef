26a3bf44ea0377b11402a8acb892413fbb0c4937
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 01:07:40 +0600

    Difficulty-319 Problem-WAITTIME

---
+++ Waiting_Time.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/WAITTIME
+int main() {
+	int test ; cin>>test ;
+	while(test--){
+	    int x ,y; cin>>x>>y;
+	    int week =x*7;
+	    cout<<week-y<<endl;
+	}
+
+}
