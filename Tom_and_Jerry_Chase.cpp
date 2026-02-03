b7d163b70c630f8835749de1dc96c6b51c088c86
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 00:08:46 +0600

    Difficulty-299 Problem-JERRYCHASE

---
+++ Tom_and_Jerry_Chase.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/JERRYCHASE
+int main(){
+    int  test ; cin>>test;
+    while (test--)
+    {
+        int a,b; cin>>a>>b;
+        if(a<b){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+      
+    return 0;
+}
