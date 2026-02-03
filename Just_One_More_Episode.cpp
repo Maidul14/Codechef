f550fef9c16b8d6e296079919c247cbbec9fc9c9
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 01:13:29 +0600

    Difficulty-320 Problem-Onemore

---
+++ Just_One_More_Episode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/ONEMORE
+int main(){
+    int test ; cin>>test;
+    while (test--)
+    {
+        int n ; cin>>n ; 
+        if(n>24){
+            cout<<"Yes"<<endl;
+        }
+        else cout<<"No"<<endl;
+    }
+      
+    return 0;
+}
