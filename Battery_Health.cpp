c06057fb87cc5e92152a5abf998ff67d498359f2
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 00:00:19 +0600

    Difficulty-297 Problem-BTRYHLTH

---
+++ Battery_Health.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/BTRYHLTH
+int main() {
+    int test ; cin>>test ;
+    while(test--){
+        int n ; cin>>n; 
+        if(n>=80){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+
+}
--- Determine_the_Score.cpp
+++ Determine_the_Score.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+//problem link : https://www.codechef.com/problems/DETSCORE
 int main(){
     int test ; cin>>test;
     while (test--)
