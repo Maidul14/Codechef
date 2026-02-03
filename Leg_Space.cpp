4c2970a7b4cb81a5e916d5bb6061f5a2fcc1e75f
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 01:22:33 +0600

    Difficulty-326 Problem-LEGSP

---
+++ Leg_Space.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/LEGSP?tab=statement
+int main(){
+    
+        int a,b; cin>>a>>b;
+        if(a<b){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+      
+    return 0;
+}
