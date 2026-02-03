662e876b656e44d0d9e48f88bacfd7085f2a729c
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 00:52:24 +0600

    Difficulty-306 Problem-MORNINGRUN

---
+++ Morning_Run.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Problem link : https://www.codechef.com/problems/MORNINGRUN
+int main(){
+	int  a,b ; cin>>a>>b;
+	int res= a*2+b*2;
+	if(res>=100){
+	    cout<<"YES"<<endl;
+	}
+	else cout<<"NO"<<endl;
+
+}
