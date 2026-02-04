b455cd1e4e0c488687129e46231c8d521cde7718
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-05 01:42:12 +0600

    Difficulty-390 Problem-MINCOINSREQ

---
+++ Minimum_Coins.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/MINCOINSREQ
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int n ; cin>>n;
+	    int coin= n%10;
+	    cout<<coin<<endl;
+	}
+
+}
