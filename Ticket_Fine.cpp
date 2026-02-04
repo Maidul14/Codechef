6a89a8630da95077dde54f9a8687aa001ccc762c
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 19:47:49 +0600

    Difficulty-373 Problem-TCKTFINE

---
+++ Ticket_Fine.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/TCKTFINE
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b,c; cin>>a>>b>>c;
+	    int res= b-c;
+	    cout<<res*a<<endl;
+	}
+
+}
