3a4956bd81dd620ac19d16441993ac73415be256
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 17:32:13 +0600

    Difficulty-354 Problem-ICE_CREAM

---
+++ ICE_CREAM.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/ICE_CREAM
+int main() {
+	int x,y ; cin>>x>>y; 
+	if(x*2<=y){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
