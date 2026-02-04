3eb80853af63a712dfe5c60b67ac67134dd675b3
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 18:01:27 +0600

    Difficulty-364 Problem-COMPLEXITY

---
+++ Time_Complexity.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/COMPLEXITY
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b; cin>>a>>b;
+	    if(a<=b){
+	        cout<<"NO"<<endl;
+	    }
+	    else if(a>b){
+	        cout<<"YES"<<endl;
+	    }
+	}
+
+}
