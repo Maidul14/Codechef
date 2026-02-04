da0c4fede6e958fd13461e259476b31a1ce860f6
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-05 02:07:35 +0600

    Difficulty-401 Problem-DISCNT

---
+++ Discount.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/DISCNT
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int n ; cin>>n;
+	    cout<<100-n<<endl;
+	}
+
+}
