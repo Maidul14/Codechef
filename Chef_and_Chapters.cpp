9540b5116cab900f8466cc3e1e0beaa35a1e2327
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 17:13:58 +0600

    Difficulty-350 Problem-SEMCOURSES

---
+++ Chef_and_Chapters.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/SEMCOURSES
+int main() {
+    int test ; cin>>test;
+    while(test--){
+        int a,b,c; cin>>a>>b>>c;
+        cout<<a*b*c<<endl;
+    }
+
+}
