aaa7c664b944aae2f385e58b84f88a8ba049ea36
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-05 01:30:20 +0600

    Difficulty-382 Problem-VIDEOWORTH

---
+++ Worth_of_a_Video.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/VIDEOWORTH
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a; cin>>a;
+	    cout<<a*24*1000<<endl;
+	}
+
+}
