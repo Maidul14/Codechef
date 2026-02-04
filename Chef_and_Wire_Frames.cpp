9f34833e2aa2d8992f30545c12f9af441a876e7e
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-05 01:36:36 +0600

    Difficulty-383 Problem-CWIREFRAME

---
+++ Chef_and_Wire_Frames.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CWIREFRAME
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b,c; cin>>a>>b>>c;
+	    int res= a*2+b*2;
+	    cout<<res*c<<endl;
+	}
+
+}
