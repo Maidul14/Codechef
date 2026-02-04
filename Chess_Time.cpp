03771f1802f07d785192b4dd038b8ae40a1830b7
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-04 16:28:26 +0600

    Difficulty-335 Problem-CHESSTIME

---
+++ Chess_Time.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CHESSTIME
+int main() {
+	int test ; cin>>test; 
+	while(test--){
+	    int n ; cin>>n;
+	    int hour = n*60;
+	    cout<<hour/20<<endl;
+	}
+
+}
