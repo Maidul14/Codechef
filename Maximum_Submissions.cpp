54f950149967fd67196204c07f98cbdc3acb961e
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-10 00:08:41 +0600

    Difficulty-435 Problem-MAXIMUMSUBS

---
+++ Maximum_Submissions.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/MAXIMUMSUBS
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int x; cin>>x;
+	    int min= x*60;
+	    cout<<min/30<<endl;
+	}
+
+}
