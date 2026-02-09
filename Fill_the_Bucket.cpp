8fd2b2a9b02e19adc08531f231bba11d62f7eb3d
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-09 23:36:34 +0600

    Difficulty-419 Problem-FBC

---
+++ Fill_the_Bucket.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/FBC
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b; cin>>a>>b;
+	    cout<<a-b<<endl;
+	}
+
+}
