902d8460d2b60730bab8b89d0de5f0ad2162ffa2
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-09 23:57:39 +0600

    Difficulty-427 Problem-MANIPULATE

---
+++ Ezio_and_Guards.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :
+int main() {
+	int test ; cin>>test ;
+	while(test--){
+	    int x,y ; cin>>x>>y;
+	    if(x>=y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
