f702d996dd58dfe70761343014879ad263e0e44b
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-12 00:56:33 +0600

    Difficulty-505 Problem-SUBSCRIBE_

---
+++ Subscriptions.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/SUBSCRIBE_
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int n,x ; cin>>n>>x;
+	    int member = (n+5)/6;
+	    int subs = member*x;
+	    cout<<subs<<endl;
+	}
+
+}
