827f3dd0267681e464ef89c6977497219e7e284e
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 14:40:07 +0600

    Difficulty-452 Problem-TYRE

---
+++ Tyre_problem.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/TYRE
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int b,c; cin>>b>>c;
+	    cout<<b*2+c*4<<endl;
+	}
+
+}
