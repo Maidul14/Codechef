cf52532ad6309eea5277beaca8245b30d8bbc674
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 15:46:14 +0600

    Difficulty-475 Problem- INSURANCE

---
+++ Insurance.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/INSURANCE
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b; cin>>a>>b;
+	    cout<<min(a,b)<<endl;
+	}
+
+}
