ee24638585afb06e0a3823ff8d4fbbaa7797b6de
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-12 02:17:18 +0600

    Difficulty-513 Problem-JASSIGNMENTS

---
+++ Janmansh_and_Assignments.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/JASSIGNMENTS
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int n ; cin>>n;
+	    if(n+3<=10){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
