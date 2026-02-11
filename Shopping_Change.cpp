8e58ea957321c9799d8ee2fad60210d63501de3b
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-12 02:37:16 +0600

    Difficulty-526 Problem-SHOPCHANGE

---
+++ Shopping_Change.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/SHOPCHANGE
+int main() {
+	int test ; cin>>test ;
+	while(test--){
+	    int a ; cin>>a;
+	    cout<<100-a<<endl;
+	}
+
+}
