8088f8cf6bf5b053af96885789a051eadb2af225
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 14:56:10 +0600

    Difficulty-459 Problem-CREDSCORE

---
+++ Credit_score.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CREDSCORE
+int main() {
+	int n ; cin>>n;
+	if(n>=750){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
