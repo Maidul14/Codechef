dbfd39ed33db1f00bfba4def20595c10eb259dce
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 20:22:25 +0600

    Difficulty-500 Problem-AVGPROBLEM

---
+++ Greater_Average.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/AVGPROBLEM
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b,c; cin>>a>>b>>c;
+	    
+	    if(a+b>c*2){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
