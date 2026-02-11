0c6fd43b99bdcbf063c599a14148aaa081420f35
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 19:33:47 +0600

    Difficulty-495 Problem-CHEFCHOCO

---
+++ Chef_and_Chocolates.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CHEFCHOCO
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b,c; cin>>a>>b>>c;
+	    int need = a-b ;
+	    cout<<need*c<<endl;
+	}
+
+}
