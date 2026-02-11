766fc91ae17b71799be35f150f48206886db877b
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 14:44:39 +0600

    Difficulty-455 Problem-PERFECTTRIO

---
+++ Perfect_Trio.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/PERFECTTRIO
+int main() {
+	int test ; cin>>test ;
+	while(test--){
+	    int a,b,c; cin>>a>>b>>c;
+	    if((a+b==c)||(b+c==a)||(c+a==b)){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
