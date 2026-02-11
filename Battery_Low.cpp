3a71ce272091c7c020a2d505fc871be26822e02f
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 16:24:49 +0600

    Difficulty-479 Problem-BATTERYLOW

---
+++ Battery_Low.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/BATTERYLOW
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int n ; cin>>n;
+	    if(n<=15){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
