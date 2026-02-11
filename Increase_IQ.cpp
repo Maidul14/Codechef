6e8f7df2fdf05a3959d1ef49a316750dd7f5cae1
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 16:21:30 +0600

    Difficulty-478 Problem-INCRIQ

---
+++ Increase_IQ.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/INCRIQ
+int main() {
+    int n ; cin>>n;
+    if(n+7>170){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+
+}
