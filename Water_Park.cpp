0c053d869f141969d630dc2d35a0a7d398d5afa0
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 16:58:58 +0600

    Difficulty- 485 Problem-SPCP1

---
+++ Water_Park.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/SPCP1
+int main() {
+    int W, H;
+    cin>>W>>H;
+    if(60 <= W && 130 >= H) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
