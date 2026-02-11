d331fac4c9637d8b429dfe3763a9ec32a95a0f75
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-11 14:53:15 +0600

    Difficulty-456 Problem-BUDGET_

---
+++ Monthly_Budget.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/BUDGET_
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int a,b; cin>>a>>b;
+	    if(b*30<=a){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
