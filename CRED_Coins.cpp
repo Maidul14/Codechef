ece319233143bb1c1ad597013b470adbb9ceabdc
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-13 00:49:31 +0600

    Difficulty-539 Problem-CREDCOINS

---
+++ CRED_Coins.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/CREDCOINS
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int x,y ; cin>>x>>y;
+	    int res = x*y ;
+	    cout<<res/100<<endl;
+	}
+
+}
