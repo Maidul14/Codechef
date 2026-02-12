0ffd080c2fca958b180aff8898b161a2c1c0abd1
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-13 01:57:59 +0600

    Difficulty-546 Problem-MINPIZZA

---
+++ Minimum_Pizzas.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/MINPIZZA
+int main() {
+	int test ; cin>>test ;
+	while(test--){
+	    int a,b; cin>>a>>b;
+	    int pizza= a*b+3;
+	    cout<<pizza/4<<endl;
+	}
+
+}
