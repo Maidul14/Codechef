848b306fb0f6437057822e43a2398ebdec6c9d8d
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-12 22:24:55 +0600

    Difficulty-532 Problem-OFFICE

---
+++ Chef_in_his_Office.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/OFFICE?tab=statement
+int main(){
+    int test ; cin>>test;
+    while(test--){
+        int x,y ; cin>>x>>y;
+        cout<<x*4+y<<endl;
+    }  
+    return 0;
+}
