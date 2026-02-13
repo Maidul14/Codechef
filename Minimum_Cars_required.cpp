73583833176e546318ad3362847307ed6420091c
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-14 01:38:09 +0600

    Difficulty-608 Problem-MINCARS

---
+++ Minimum_Cars_required.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/MINCARS?tab=ide
+int main(){
+    int test ; cin >> test;
+    while(test--){
+        int n ; cin >> n;
+        int need= (n+3)/4;
+        cout << need << endl;
+    }  
+    return 0;
+}
