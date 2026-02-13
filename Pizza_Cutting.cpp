c7617b1422c8591923779eabfe72122db514c64c
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-13 14:52:42 +0600

    Difficulty-566 Problem-PIZZAC

---
+++ Pizza_Cutting.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/PIZZAC
+int main() {
+	int test ; cin>>test ;
+	while(test--){
+	    int x ; cin>>x;
+	    if(x==1){
+	        cout<<"YES"<<endl;
+	    }
+	    else if(x%2==0){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
