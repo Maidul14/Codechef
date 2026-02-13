93458755cd70bdfc6d55848b85fd4625c5e32a7b
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-13 17:04:32 +0600

    Difficulty-588 Problem-FLOW007

---
+++ Reverse_The_Number.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/FLOW007
+int main() {
+	 int test ; cin>>test;
+	 while(test--){
+	     string s ; cin>>s;
+	     reverse(s.begin(),s.end());
+         int x = stoi(s);  
+	     cout<<x<<endl;
+	 }
+
+}
