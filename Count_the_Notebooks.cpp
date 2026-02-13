bd5edf5b6c939801a5e92dc7f7b2d65890d0c973
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-13 14:38:22 +0600

    Difficulty-563 Problem-NOTEBOOK

---
+++ Count_the_Notebooks.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/NOTEBOOK
+int main() {
+	int test ; cin>>test;
+	while(test--){
+	    int x  ; cin>>x;
+	    int page = x*1000;
+	    int note = page/100;
+	    cout<<note<<endl;
+	}
+
+}
