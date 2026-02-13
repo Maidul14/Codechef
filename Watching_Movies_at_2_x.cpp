b7100063def4f081a1b4be51a02e3711e3df6028
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-14 02:24:42 +0600

    Difficulty-628 Problem-MOVIE2X

---
+++ Watching_Movies_at_2_x.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/MOVIE2X?tab=ide
+int main(){
+        int a,b ; cin>>a>>b;
+        int m= b/2;
+        cout<<a-m<<endl;
+    return 0;
+}
