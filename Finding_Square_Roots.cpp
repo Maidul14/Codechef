c5e7682964b97f5a4cf84b34d4c5ae4446ca026d
maidul14 <md.maidulislammahin14@gmail.com>
2026-02-15 13:54:23 +0600

    Difficulty-660 Problem-FSQRT

---
+++ Finding_Square_Roots.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link :https://www.codechef.com/problems/FSQRT
+int main() {
+	int test ; cin>>test ;
+	while(test--){
+	    int n ; cin>>n;
+	    int rt = sqrt(n) ;
+	    cout<<rt<<endl;
+	}
+
+}
